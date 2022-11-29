7e7c92491329540c172b2055d2fa0026c8219b1b
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-11-29 07:29:38 +0100

    initial commit

---
+++ spec/todo_cmd_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TodoCmd do
+  it "has a version number" do
+    expect(TodoCmd::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end

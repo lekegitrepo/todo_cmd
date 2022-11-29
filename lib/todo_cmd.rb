5f828db4a8c1bd86f65416c1a20ffee9004cf265
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-11-29 07:45:06 +0100

    Simple initial logic

--- lib/todo_cmd.rb
+++ lib/todo_cmd.rb
@@ -4,5 +4,22 @@ require_relative "todo_cmd/version"
 
 module TodoCmd
   class Error < StandardError; end
-  # Your code goes here...
+  
+  TODO_FILE = 'todo.txt'
+
+  def read_todo(line)
+    line.chomp.split(/,/)
+  end
+  
+  def write_todo(file, name, created = Time.now, completed = '')
+    file.puts "#{name}, #{created}, #{completed}"
+  end
+
+  command = ARGV.shift
+
+  case command
+  when 'new'
+  when 'list'
+  when 'done'
+  end
 end

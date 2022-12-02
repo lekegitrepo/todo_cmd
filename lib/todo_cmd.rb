d4b533b80790de3ccda02da87a76e37d301c5938
Temitayo Adeleke <lekedevrepo@gmail.com>
2022-12-02 09:03:06 +0100

    Add done command logic

--- lib/todo_cmd.rb
+++ lib/todo_cmd.rb
@@ -38,5 +38,22 @@ module TodoCmd
       end
     end
   when 'done'
+    task_number = ARGV.shift.to_i
+
+    File.open(TODO_FILE, 'r') do |file|
+      File.open("#{TODO_FILE}.new", 'w') do |new_file|
+      counter = 1
+        name, created, completed = read_todo(file)
+        
+        if counter == task_number
+          write_todo(new_file, name, created, Time.now)
+          puts "Task #{counter} completed"
+        else
+          write_todo(new_file, name, created, completed)
+        end
+        counter += 1
+      end
+    end
   end
+  `mv #{TODO_FILE}.new #{TODO_FILE}`
 end
